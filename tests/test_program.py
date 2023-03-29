5567edb79e84ae8d8ec28dc9e85dd5e87a8c2197
goldi4ek <2002mdo@gmail.com>
2023-03-29 14:51:12 +0300

    created all tests

--- tests/test_program.py
+++ tests/test_program.py
@@ -25,3 +25,15 @@ def test_check_different(file_checker):
     result = file_checker.check_different(file1, file2)
     assert result == {"me", "nobody", "love"}
 
+
+@pytest.mark.parametrize('input_line, result_line', [("hello", {"hello"}),
+                                                     ("buy", {"buy"}),
+                                                     ("hay", {"hay"}),
+                                                     ("dota2", {"dota2"})])
+def test_check_similar_parametrize(file_checker, input_line, result_line):
+    file_checker.wrote_file([input_line], "testCheckSimilarParametrize1")
+    file_checker.wrote_file([input_line], "testCheckSimilarParametrize2")
+    text1 = file_checker.open_file("testCheckSimilarParametrize1.txt")
+    text2 = file_checker.open_file("testCheckSimilarParametrize2.txt")
+    result = file_checker.check_similar(text1, text2)
+    assert result == result_line
