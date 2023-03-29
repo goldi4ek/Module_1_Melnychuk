25143dcecdbe9293b8434a2604d5d0ac53e072eb
goldi4ek <2002mdo@gmail.com>
2023-03-29 14:15:09 +0300

    created check_similar function

--- main.py
+++ main.py
@@ -9,11 +9,18 @@ def open_file(name):
         linesInFile.append(line.replace('\n', ''))
     return linesInFile
 
+
+def check_similar(file1, file2):
+    return set(file1).intersection(file2)
+
+
+
 def main():
     file1 = open_file("inputFile1.txt")
     file2 = open_file("inputFile2.txt")
     print(file1)
     print(file2)
+    similar_lines = check_similar(file1, file2)
 
 
 if __name__ == "__main__":
