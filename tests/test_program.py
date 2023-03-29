4686e49ef91f58ace7a4bf08d3021814e270f867
goldi4ek <2002mdo@gmail.com>
2023-03-29 14:37:59 +0300

    created two more tests

--- tests/test_program.py
+++ tests/test_program.py
@@ -10,3 +10,18 @@ def file_checker():
 def test_input(file_checker):
     result = file_checker.open_file("testinput.txt")
     assert result == ['hello', 'everyone']
+
+
+def test_check_similar(file_checker):
+    file1 = file_checker.open_file("testSimilarDifferenceFile1.txt")
+    file2 = file_checker.open_file("testSimilarDifferenceFile2.txt")
+    result = file_checker.check_similar(file1, file2)
+    assert result == {"hello", "everyone", "guys"}
+
+
+def test_check_different(file_checker):
+    file1 = file_checker.open_file("testSimilarDifferenceFile1.txt")
+    file2 = file_checker.open_file("testSimilarDifferenceFile2.txt")
+    result = file_checker.check_different(file1, file2)
+    assert result == {"me", "nobody", "love"}
+
